5c8a7b51193164c9832d543984e5aa80ad5a1302
rurumimic <unhyop@gmail.com>
2020-05-12 23:38:08 +0900

    작은 수정

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
   root: true,
   plugins: [
-    'jsx-a11y',
-    'prettier',
+    "jsx-a11y",
+    "prettier",
   ],
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
   },
   extends: [
-    'react-app',
-    'standard-with-typescript',
-    'plugin:jsx-a11y/recommended',
-    'prettier',
-    'prettier/react',
-    'prettier/standard',
-    'prettier/@typescript-eslint',
+    "react-app",
+    "standard-with-typescript",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/standard",
+    "prettier/@typescript-eslint",
   ],
   rules: {
-    'prettier/prettier': 'error'
-  }
-}
+    "prettier/prettier": "error",
+  },
+};
